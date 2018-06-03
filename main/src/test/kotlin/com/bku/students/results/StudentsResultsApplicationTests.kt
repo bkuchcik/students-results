4c126f3fa49e8b4f34921d24a9310ab795fef5b2
Benjamin KUCHCIK <bkuchcik.coursjee@gmail.com>
2018-06-03 23:31:58 +0200

    Bootstrap project structure with three project, business independant and persistence as an adaptator

---
+++ main/src/test/kotlin/com/bku/students/results/StudentsResultsApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.bku.students.results
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class StudentsResultsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
